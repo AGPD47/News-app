89028425ac49d8e28716598241d94b17704d315b
Priyanshu Dharmshaktu <dharmshaktu23@gmail.com>
2024-07-01 15:57:48 +0530

    first commit

---
+++ src/components/NewsItem.jsx
@@ -0,0 +1,16 @@
+
+
+const NewsItem = ({title, description, src, url}) => {
+  return (
+    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth: "300px"}}>
+    <img  src={src?src:'./image.jpeg'} style={{height:"200px", width:"285px"}} className="card-img-top img" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">{title.slice(0,100)}</h5>
+      <p className="card-text">{description? description.slice(0,100):""}</p>
+      <a href={url} className="btn btn-primary">Read More</a>
+    </div>
+  </div>
+  )
+}
+
+export default NewsItem;
